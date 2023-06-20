f6473d2dbb7292973e334fcd21adfebd07eed777
Smit90 <45687440+Smit90@users.noreply.github.com>
2023-06-20 15:04:03 +0530

    added multiple navigators

--- App.js
+++ App.js
@@ -2,11 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#351401",
+        },
+        headerTintColor: "white",
+        sceneContainerStyle: {
+          backgroundColor: "#3f2f25",
+        },
+      }}
+    >
+      <Drawer.Screen
+        component={CategoriesScreen}
+        name="Categories"
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen component={FavoriteScreen} name="Favorites" />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -25,10 +51,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen
